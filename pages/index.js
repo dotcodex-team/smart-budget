5aff3795e0ee23562944766fdbd23e94b035b14a
Juan Rivera Vargas <jrivera.v@outlook.com>
2020-04-18 00:37:41 -0400

    hooks example

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,50 @@
-import React from 'react'
+import React, { useState,useEffect,useMemo,useContext } from 'react'
 import { Button } from 'semantic-ui-react'
 import { createUseStyles } from 'react-jss'
 
 const useStyles = createUseStyles({
-
+  root : {
+    padding : 15,
+    background : '#f8f8f8',
+    width : 340
+  }
 })
 
 export default function Home() {
   const classes = useStyles();
+  const valor = 10;
+  //data
+  const [ number, setNumber ] = useState(0);
+  const [ status, setStatus ] = useState(false);
+
+
+
+  //computed
+  const numberAndValor = useMemo(()=>number + valor,[ number ])
+
+  //mounted
+  useEffect(()=>{
+    increase();
+  },[ status ])
+
+  //method
+  function increase(){
+    setNumber(number + 1)
+  }
+
   return (
-    <div>
-      <Button primary>Hola Mundo</Button>
+    <div className={classes.root}>
+      <Button onClick={increase} primary>
+        {numberAndValor}
+      </Button>
+      <br />
+      <br />
+      <div>
+        <Button onClick={()=>setStatus(!status)} primary>
+          Cambiar el status activa el efecto
+        </Button>
+        <span> { status ? 'Activo' : 'Inactivo' } </span>
+      </div>
     </div>
   )
 }
