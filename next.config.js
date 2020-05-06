5ebf8133991d42690e0428e57585448ef6151d3e
Juan Rivera Vargas <jrivera.v@outlook.com>
2020-05-05 22:03:46 -0400

    layout alias

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
   webpack: config => {
     config.resolve.alias['~'] = path.resolve(__dirname);
     config.resolve.alias['@components'] = path.resolve(__dirname, 'components');
-    config.resolve.alias['@config'] = path.resolve(__dirname, 'config');
+    config.resolve.alias['@layout'] = path.resolve(__dirname, 'layout');
     return config;
   }
 };
