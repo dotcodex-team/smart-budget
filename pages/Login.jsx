6268ad819e1746a69983484f99bf8d66baade962
carlos <carlos.20499@gmail.com>
2020-05-05 20:09:32 -0400

    login responsive

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,23 +1,52 @@
 import React from 'react';
-import { Container, makeStyles } from '@material-ui/core';
+import { Container, makeStyles, Grid, Card } from '@material-ui/core';
 import SignIn from '../components/login/SignIn';
 import SignUp from '../components/login/SignUp';
 
 const useStyle = makeStyles({
   root: {
-    background: 'green',
-    color: 'blue'
+    background: '#F0F4F3',
+    color: 'blue',
+    height: '100vh',
+    width: '100%',
+    padding: '0 7%'
+  },
+  grid1: {
+    //  background: '#BFDAE1',
+    color: 'white',
+    height: 'calc(100% - 100px)',
+    borderTopRightRadius: 0
+  },
+  grid2: {
+    background: '#4EB1B6',
+    color: 'white',
+    height: 'calc(100% - 100px)',
+    borderTopLeftRadius: 0
+  },
+  signInContainer: {
+    height: '100%'
   }
 });
 
 export default function Login() {
-  const style = useStyle();
+  const classes = useStyle();
   return (
     <>
-      <Container className={style.root}>
-        <SignIn></SignIn>
-        <SignUp></SignUp>
-      </Container>
+      <Grid container alignItems="center" className={classes.root}>
+        <Grid component={Card} item xs={12} md={7} className={classes.grid1}>
+          <Grid
+            container
+            justify="center"
+            alignItems="center"
+            className={classes.signInContainer}
+          >
+            <SignIn></SignIn>
+          </Grid>
+        </Grid>
+        <Grid component={Card} item xs={12} md={5} className={classes.grid2}>
+          <SignUp></SignUp>
+        </Grid>
+      </Grid>
     </>
   );
 }
